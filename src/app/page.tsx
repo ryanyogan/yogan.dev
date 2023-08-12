6bb8c485c205f666551d7ea3b3e0c6540530b116
Ryan Yogan <ryanyogan@gmail.com>
2023-08-12 14:31:45 -0500

    article // tweak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,8 +74,8 @@ export default function HomePage() {
       <div className="my-8 flex flex-col space-y-4 w-full">
         <Suspense>
           <BlogLink
-            name="This will be a fun year as all I want to do is educate."
-            slug="hello-yogi"
+            name="New Year Starting in August"
+            slug="new-year-starting-in-august"
           />
         </Suspense>
       </div>
