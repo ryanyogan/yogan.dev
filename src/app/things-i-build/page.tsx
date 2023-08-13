c6572c560a05918c3c1993e59f2d579432a6e056
Ryan Yogan <ryanyogan@gmail.com>
2023-08-12 23:09:13 -0500

    nicer external links

--- src/app/things-i-build/page.tsx
+++ src/app/things-i-build/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { ExternalLink } from "@/components/shared/external-link";
 
 export const runtime = "edge";
 export default function TechToys() {
@@ -10,144 +10,84 @@ export default function TechToys() {
           to learn, for fun.
         </span>
       </h1>
-      <Link
-        href="https://jarvis.yogan.dev"
-        target="_blank"
-        className="flex flex-col space-y-1 mb-4"
-      >
-        <div className="w-full flex flex-col">
-          <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-            Jarvis
-          </p>
-          <p className="text-neutral-600 dark:text-neutral-400">
+
+      <div className="my-8 flex flex-col space-y-4 w-full">
+        <ExternalLink
+          title="Jarvis"
+          description="
             AI Chat Bot powered by NextJS, OpenAI, Redis KV, and Server
             Streaming. This shows off the latest power of Next & Vercel
             combined, I love it!
-          </p>
-        </div>
-      </Link>
-      <Link
-        href="https://friend.yogan.dev"
-        target="_blank"
-        className="flex flex-col space-y-1 mb-4"
-      >
-        <div className="w-full flex flex-col">
-          <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-            Build. A. Friend.
-          </p>
-          <p className="text-neutral-600 dark:text-neutral-400">
-            Select a pre-modeled character to chat with, or create your own
-            witty individual to have chat with :)
-          </p>
-        </div>
-      </Link>
-      <Link
-        href="https://egg.yogan.dev"
-        target="_blank"
-        className="flex flex-col space-y-1 mb-4"
-      >
-        <div className="w-full flex flex-col">
-          <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-            Egg O&apos; Wisdom
-          </p>
-          <p className="text-neutral-600 dark:text-neutral-400">
-            Using the power of LLM&apos; and vector DB&apos; this fun service
-            allows you to select the topic, and it will generate either multiple
-            choice, or an open-ended quiz.
-          </p>
-        </div>
-      </Link>
-      <Link
-        href="https://yogi.yogan.dev/dashboard"
-        target="_blank"
-        className="flex flex-col space-y-1 mb-4"
-      >
-        <div className="w-full flex flex-col">
-          <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-            Yogi
-          </p>
-          <p className="text-neutral-600 dark:text-neutral-400">
-            NextJS, Clerk, Prisma, PlanetScale, Tailwind. Multi-Purpose GPT,
-            DALL-E, Music Generation, Video Generation, Code Generation.
-          </p>
-        </div>
-      </Link>
-      <Link
-        href="https://one.yogan.dev"
-        target="_blank"
-        className="flex flex-col space-y-1 mb-4"
-      >
-        <div className="w-full flex flex-col">
-          <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-            Here&apos; One Thing
-          </p>
-          <p className="text-neutral-600 dark:text-neutral-400">
-            Fun little NextJs, Prisma, PlanetScale, Typescript Meta Messaenger
-            Clone
-          </p>
-        </div>
-      </Link>
-      <Link
-        href="https://bnb.ryanyogan.com"
-        target="_blank"
-        className="flex flex-col space-y-1 mb-4"
-      >
-        <div className="w-full flex flex-col">
-          <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-            AirBNB Clone
-          </p>
-          <p className="text-neutral-600 dark:text-neutral-400">
-            What the internet be without yet another Next13 app router based
-            AirBNB clone?!
-          </p>
-        </div>
-      </Link>
-      <Link
-        href="https://store.ryanyogan.com"
-        target="_blank"
-        className="flex flex-col space-y-1 mb-4"
-      >
-        <div className="w-full flex flex-col">
-          <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-            E-Commerce Store
-          </p>
-          <p className="text-neutral-600 dark:text-neutral-400">
-            This one is fun, it uses some neat tricks, it simply is an example
-            store front for the SaaS / Ecommerce backend.
-          </p>
-        </div>
-      </Link>
-      <Link
-        href="https://ecommerce.yogan.dev"
-        target="_blank"
-        className="flex flex-col space-y-1 mb-4"
-      >
-        <div className="w-full flex flex-col">
-          <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-            E-Commerce Admin
-          </p>
-          <p className="text-neutral-600 dark:text-neutral-400">
-            A really fun NextJS / ShadCN / TS / Prisma / Planetscale service
-            exposing a rest and GQL api for both the web and native application.
-          </p>
-        </div>
-      </Link>
-      <Link
-        href="https://fly.yogan.dev"
-        target="_blank"
-        className="flex flex-col space-y-1 mb-4"
-      >
-        <div className="w-full flex flex-col">
-          <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-            V REF
-          </p>
-          <p className="text-neutral-600 dark:text-neutral-400">
-            Serious Flight Simmer? Then you want to use this for your flight
-            planning needs. In time, it is my hope that this can become the
-            ForeFlight&apos; of Simming!
-          </p>
-        </div>
-      </Link>
+          "
+          href="https://jarvis.yogan.dev"
+        />
+        <ExternalLink
+          title="Build a Friend"
+          description="
+          Select a pre-modeled character to chat with, or create your own
+          witty individual to have chat with :)
+          "
+          href="https://friend.yogan.dev"
+        />
+        <ExternalLink
+          title="Egg Of Wisdom"
+          description="
+          Using the power of LLM' and vector DB' this fun service
+          allows you to select the topic, and it will generate either multiple
+          choice, or an open-ended quiz.
+          "
+          href="https://egg.yogan.dev"
+        />
+        <ExternalLink
+          title="Yogi"
+          description="
+          NextJS, Clerk, Prisma, PlanetScale, Tailwind. Multi-Purpose GPT,
+          DALL-E, Music Generation, Video Generation, Code Generation.
+          "
+          href="https://yogi.yogan.dev"
+        />
+        <ExternalLink
+          title="Here's One Thing"
+          description="
+          Fun little NextJs, Prisma, PlanetScale, Typescript Meta Messaenger
+          Clone
+          "
+          href="https://one.yogan.dev"
+        />
+        <ExternalLink
+          title="AirBnB Clone"
+          description="
+          What would the internet be without yet another Next13 app router based
+          AirBNB clone?!
+          "
+          href="https://bnb.ryanyogan.com"
+        />
+        <ExternalLink
+          title="E-Commerce Store"
+          description="
+          This one is fun, it uses some neat tricks, it simply is an example
+          store front for the SaaS / Ecommerce backend.
+          "
+          href="https://store.yogan.dev"
+        />
+        <ExternalLink
+          title="E-Commerce SaaS Admin"
+          description="
+          A really fun NextJS / ShadCN / TS / Prisma / Planetscale service
+          exposing a rest and GQL api for both the web and native application.
+          "
+          href="https://ecommerce.yogan.dev"
+        />
+        <ExternalLink
+          title="V-SYNC"
+          description="
+          Serious Flight Simmer? Then you want to use this for your flight
+          planning needs. In time, it is my hope that this can become the
+          ForeFlight' of Simming!
+          "
+          href="https://vsync.yogan.dev"
+        />
+      </div>
     </section>
   );
 }
---
+++ src/components/shared/external-link.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { ArrowIcon } from "./arrow-icon";
+
+interface ExternalLinkProps {
+  title: string;
+  description: string;
+  href: string;
+}
+
+export function ExternalLink({ title, href, description }: ExternalLinkProps) {
+  return (
+    <Link
+      target="_blank"
+      href={href}
+      className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4 w-full"
+    >
+      <div className="flex flex-col mr-2">
+        <p className="font-bold text-neutral-900 dark:text-neutral-100">
+          {title}
+        </p>
+
+        <p className="text-neutral-600 dark:text-neutral-400">{description}</p>
+      </div>
+      <div className="text-neutral-700 dark:text-neutral-300">
+        <ArrowIcon />
+      </div>
+    </Link>
+  );
+}
