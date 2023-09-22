28548ca7cfa4f7ae4f2a974998b97da15277d3c1
Ryan Yogan <ryanyogan@gmail.com>
2023-09-22 16:53:29 -0500

    feat // more silly stuff

--- src/app/our-zoo/page.tsx
+++ src/app/our-zoo/page.tsx
@@ -2,11 +2,30 @@ import { getRecentImages } from "@/actions";
 import BlurImage from "./_components/blur-image";
 import UploadContent from "./_components/upload-content";
 
+export const runtime = "edge";
+
 export default async function OurZoo() {
   const images = await getRecentImages();
 
   return (
-    <div>
+    <section>
+      <h1 className="font-bold text-2xl mb-8 tracking-tighter">
+        the zoo,{" "}
+        <span className="text-neutral-400 font-normal">
+          we love our animals!
+        </span>
+      </h1>
+
+      <br />
+
+      <p className="prose prose-neutral dark:prose-invert">
+        I am fairly certain we have the oddest cats on this planet, have yet to
+        check others 🚀. We take way too many damn pictures of these little
+        stinkers. 🙀
+      </p>
+
+      <br />
+
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6">
         {images.length
           ? images.map((image) => <BlurImage key={image.id} image={image} />)
@@ -16,6 +35,6 @@ export default async function OurZoo() {
       <div className="mt-20">
         <UploadContent />
       </div>
-    </div>
+    </section>
   );
 }
